e5532800a59e76bde99786649bab10abbf20b829
Rean-S <63460284+Rean-S@users.noreply.github.com>
2021-10-14 13:27:47 +0800

    Initial commit

---
+++ app/src/main/java/com/dataflair/musicplayer/Activities/PlayerActivity.java
@@ -0,0 +1,306 @@
+package com.dataflair.musicplayer.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dataflair.musicplayer.R;
+import com.gauravk.audiovisualizer.visualizer.BarVisualizer;
+import com.gauravk.audiovisualizer.visualizer.BlastVisualizer;
+import com.gauravk.audiovisualizer.visualizer.CircleLineVisualizer;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    Button btnPlay, btnNext, btnPrevious, btnFastForward, btnFastBackWard;
+    TextView txtSongName, txtSongStart, txtSongEnd;
+    SeekBar seekMusicBar;
+    BarVisualizer barVisualizer;
+
+
+    ImageView imageView;
+
+
+    String songName;
+    public static final String EXTRA_NAME = "song_name";
+    static MediaPlayer mediaPlayer;
+    int position;
+
+    ArrayList<File> mySongs;
+
+    Thread updateSeekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        btnPlay = (Button) findViewById(R.id.BtnPlay);
+        btnNext = (Button) findViewById(R.id.BtnNext);
+        btnPrevious = (Button) findViewById(R.id.BtnPrevious);
+        btnFastForward = (Button) findViewById(R.id.BtnFastForward);
+        btnFastBackWard = (Button) findViewById(R.id.BtnFastRewind);
+
+
+        txtSongName = (TextView) findViewById(R.id.SongTxt);
+        txtSongStart = (TextView) findViewById(R.id.TxtSongStart);
+        txtSongEnd = (TextView) findViewById(R.id.TxtSongEnd);
+
+        seekMusicBar = (SeekBar) findViewById(R.id.SeekBar);
+        barVisualizer = (BarVisualizer) findViewById(R.id.wave);
+
+        imageView = (ImageView) findViewById(R.id.MusicImage);
+        if (mediaPlayer != null) {
+
+            mediaPlayer.start();
+            mediaPlayer.release();
+        }
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        mySongs = (ArrayList) bundle.getIntegerArrayList("songs");
+        String sName = intent.getStringExtra("songname");
+        position = bundle.getInt("pos");
+        txtSongName.setSelected(true);
+
+        Uri uri = Uri.parse(mySongs.get(position).toString());
+        songName = mySongs.get(position).getName();
+        txtSongName.setText(songName);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+        mediaPlayer.start();
+
+        songEndTime();
+
+        visualizer();
+
+
+        updateSeekBar = new Thread() {
+            @Override
+            public void run() {
+
+                int TotalDuration = mediaPlayer.getDuration();
+                int CurrentPosition = 0;
+
+                while (CurrentPosition < TotalDuration) {
+                    try {
+
+                        sleep(500);
+                        CurrentPosition = mediaPlayer.getCurrentPosition();
+                        seekMusicBar.setProgress(CurrentPosition);
+
+                    } catch (InterruptedException | IllegalStateException e) {
+
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        };
+
+        seekMusicBar.setMax(mediaPlayer.getDuration());
+        updateSeekBar.start();
+
+        seekMusicBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                mediaPlayer.seekTo(seekBar.getProgress());
+
+            }
+        });
+
+
+        final Handler handler = new Handler();
+        final int delay = 1000;
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                String currentTime = createDuration(mediaPlayer.getCurrentPosition());
+
+                txtSongStart.setText(currentTime);
+                handler.postDelayed(this, delay);
+
+            }
+        }, delay);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (mediaPlayer.isPlaying()) {
+
+                    btnPlay.setBackgroundResource(R.drawable.play_song_icon);
+
+                    mediaPlayer.pause();
+
+                } else {
+
+                    btnPlay.setBackgroundResource(R.drawable.pause_song_icon);
+
+                    mediaPlayer.start();
+
+                    TranslateAnimation moveAnim = new TranslateAnimation(-25, 25, -25, 25);
+                    moveAnim.setInterpolator(new AccelerateInterpolator());
+                    moveAnim.setDuration(600);
+                    moveAnim.setFillEnabled(true);
+                    moveAnim.setFillAfter(true);
+                    moveAnim.setRepeatMode(Animation.REVERSE);
+                    moveAnim.setRepeatCount(1);
+
+                    visualizer();
+                }
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                btnNext.performClick();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mediaPlayer.stop();
+                mediaPlayer.release();
+
+                position = ((position + 1) % mySongs.size());
+
+                Uri uri1 = Uri.parse(mySongs.get(position).toString());
+
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), uri1);
+
+                songName = mySongs.get(position).getName();
+                txtSongName.setText(songName);
+
+                mediaPlayer.start();
+
+                songEndTime();
+
+                visualizer();
+
+
+            }
+        });
+
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mediaPlayer.stop();
+                mediaPlayer.release();
+
+                position = ((position - 1) % mySongs.size());
+                if (position < 0)
+                    position = mySongs.size() - 1;
+
+                Uri uri1 = Uri.parse(mySongs.get(position).toString());
+
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), uri1);
+                songName = mySongs.get(position).getName();
+                txtSongName.setText(songName);
+                mediaPlayer.start();
+                songEndTime();
+                visualizer();
+
+            }
+
+        });
+
+        btnFastForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+
+                    mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 10000);
+
+                }
+            }
+        });
+
+        btnFastBackWard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+
+                    mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 10000);
+
+                }
+            }
+        });
+    }
+
+    public String createDuration(int duration) {
+
+        String time = "";
+        int min = duration / 1000 / 60;
+        int sec = duration / 1000 % 60;
+
+        time = time + min + ":";
+
+        if (sec < 10) {
+
+            time += "0";
+
+        }
+        time += sec;
+        return time;
+
+    }
+
+    public void visualizer() {
+
+        int audioSessionId = mediaPlayer.getAudioSessionId();
+        if (audioSessionId != -1) {
+            barVisualizer.setAudioSessionId(audioSessionId);
+        }
+    }
+
+    public void songEndTime() {
+        String endTime = createDuration(mediaPlayer.getDuration());
+        txtSongEnd.setText(endTime);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (barVisualizer != null)
+            barVisualizer.release();
+    }
+}
---
+++ app/src/main/java/com/dataflair/musicplayer/MainActivity.java
@@ -0,0 +1,138 @@
+package com.dataflair.musicplayer;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.dataflair.musicplayer.Activities.PlayerActivity;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ListView listView;
+    String[] items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.MusicList);
+
+        runTimePermission();
+    }
+
+    public void runTimePermission() {
+        Dexter.withContext(getApplicationContext())
+                .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        displaySong();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+
+    }
+
+
+    public ArrayList<File> findSong(File file) {
+
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] files = file.listFiles();
+
+        for (File singleFile : files) {
+
+            if (singleFile.isDirectory() && !singleFile.isHidden()) {
+
+                arrayList.addAll(findSong(singleFile));
+
+            } else {
+                if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")) {
+                    arrayList.add(singleFile);
+                }
+            }
+        }
+
+        return arrayList;
+    }
+
+    public void displaySong() {
+        System.out.println(Environment.getExternalStorageDirectory());
+        @SuppressLint("SdCardPath") final ArrayList<File> mySongs = findSong(new File("/mnt/sdcard"));
+        items = new String[mySongs.size()];
+
+        for (int i = 0; i < mySongs.size(); i++) {
+            items[i] = mySongs.get(i).getName().replace(".mp3", "").replace(".wav", "");
+        }
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String songName = (String) listView.getItemAtPosition(i);
+
+                Intent intent = new Intent(getApplicationContext(), PlayerActivity.class);
+                intent.putExtra("songs", mySongs);
+                intent.putExtra("songname", songName);
+                intent.putExtra("pos", i);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.song_name_layout, null);
+            TextView txtSong = (TextView) view.findViewById(R.id.SongName);
+            txtSong.setSelected(true);
+            txtSong.setText(items[position]);
+            return view;
+        }
+    }
+
+}
